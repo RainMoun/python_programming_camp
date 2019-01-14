faf3b5477101a43f011834788e38a6b48018ca76
RainMoun <775653143@qq.com>
2019-01-14 15:00:37 +0800

    finished

--- day13/session_13.py
+++ day13/session_13.py
@@ -17,6 +17,28 @@ class Student:
             return None
 
 
+def someone_score(input_name):
+    print("Name: {}".format(input_name))
+    if input_name in std_lst.keys():
+        std_lst[input_name].print_all_score()
+
+
+def everybody_subject_score(subject):
+    for j in std_lst:
+        if std_lst[j].get_score(subject):
+            print("Name: {}, {} score: {}".format(std_lst[j].name, subject, std_lst[j].get_score(subject)))
+        else:
+            print("Name: {}, {} score: Not recorded".format(std_lst[j].name, subject))
+
+
+def total_average_score(subject):
+    sum_score = []
+    for j in std_lst:
+        if std_lst[j].get_score(subject):
+            sum_score.append(std_lst[j].get_score(subject))
+    print("Subject {} average score: {}".format(subject, sum(sum_score) / len(sum_score)))
+
+
 if __name__ == '__main__':
     # 初始定义部分
     std1 = Student("Kobe")
@@ -29,26 +51,13 @@ if __name__ == '__main__':
     std2.score['rebound'] = 90
     std_lst = {"Kobe": std1, "James": std2}
     # 根据姓名查看所有成绩
-    input_name = 'Kobe'
-    print("Name: {}".format(input_name))
-    if input_name in std_lst.keys():
-        std_lst[input_name].print_all_score()
+    someone_score("Kobe")
     print("###########################################################################")
     # 查看所有人的某学科成绩
-    subject = 'shoot'
-    for j in std_lst:
-        if std_lst[j].get_score(subject):
-            print("Name: {}, {} score: {}".format(std_lst[j].name, subject, std_lst[j].get_score(subject)))
-        else:
-            print("Name: {}, {} score: Not recorded".format(std_lst[j].name, subject))
+    everybody_subject_score("shoot")
     print("###########################################################################")
     # 查看总平均分
-    subject = 'shoot'
-    sum_score = []
-    for j in std_lst:
-        if std_lst[j].get_score(subject):
-            sum_score.append(std_lst[j].get_score(subject))
-    print("Subject {} average score: {}".format(subject, sum(sum_score)/len(sum_score)))
+    total_average_score("shoot")
     print("###########################################################################")
     # 查看某人学科成绩
     selected_name = "Kobe"
