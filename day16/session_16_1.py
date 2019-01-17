88f6068ac69a2f5bf5699cf2a916ddc1f0fa1e3f
RainMoun <775653143@qq.com>
2019-01-17 08:38:16 +0800

    finished

--- day16/session_16_1.py
+++ day16/session_16_1.py
@@ -5,12 +5,12 @@ class CarMeta(type):
     def __call__(self, *args, **kwargs):  # 调用Car类的时候由于Car类没有__call__方法，因此调用CarMeta中的__call__方法
         obj = object.__new__(self)  # 创建一个空对象
         self.__init__(obj, *args, **kwargs)  # 初始化CarMeta,等同于产生Foo类
-        if 'production_date' not in dir(obj):  # 判断Foo类中是否有production_date参数，下同
-            raise TypeError('请定义production_date属性')
-        if 'engine_number' not in dir(obj):
-            raise TypeError('请定义engine_number属性')
-        if 'capacity' not in dir(obj):
-            raise TypeError('请定义capacity属性')
+        if ('production_date' and 'engine_number' and 'capacity') not in dir(obj):  # 判断Foo类中是否有production_date参数，下同
+            raise TypeError('类中属性缺失，请检查production_date、engine_number、capacity是否在类中')
+        # if 'engine_number' not in dir(obj):
+        #     raise TypeError('请定义engine_number属性')
+        # if 'capacity' not in dir(obj):
+        #     raise TypeError('请定义capacity属性')
         return obj
 
 
